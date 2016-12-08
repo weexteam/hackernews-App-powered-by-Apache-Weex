13d300bc7e68b913b27739487be7644a61811a31
Hanks <zhanghan.me@gmail.com>
2016-12-08 18:33:32 +0800

    [ios] create an empty iOS project

---
+++ ios/HackerNews/HackerNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackerNews
+//
+//  Created by Hanks on 16/12/8.
+//  Copyright © 2016年 Weex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios/HackerNews/HackerNews/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HackerNews
+//
+//  Created by Hanks on 16/12/8.
+//  Copyright © 2016年 Weex. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
