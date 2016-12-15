14922ceb40bc7cbd35d83adcd67f57f12e6330b9
Hanks <zhanghan.me@gmail.com>
2016-12-15 16:48:47 +0800

    [vue] add artice view

---
+++ src/views/ArticleView.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <div class="article-view"> -->
+    <web class="webview" :src="url"></web>
+  <!-- </div> -->
+</template>
+
+<script>
+  module.exports = {
+    props: {
+      url: {
+        type: String,
+        required: true,
+        default: 'http://www.alibaba.com/'
+      }
+    }
+  }
+</script>
