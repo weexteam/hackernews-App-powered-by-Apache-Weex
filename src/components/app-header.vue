541bb7232f933646d91084a89756db8051d2c6f0
Hanks <zhanghan.me@gmail.com>
2016-12-22 00:34:20 +0800

    update app header

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="app-header">
-    <image class="app-logo" src="https://news.ycombinator.com/favicon.ico"></image>
-    <text class="app-title">{{title}}</text>
+  <div class="header">
+    <router-link class="logo" to="/" exact>
+      <image class="image" src="https://news.ycombinator.com/favicon.ico"></image>
+    </router-link>
+    <div class="nav">
+      <router-link class="link" to="/top">
+        <text class="title">Top</text>
+      </router-link>
+      <router-link class="link" to="/new">
+        <text class="title">New</text>
+      </router-link>
+      <router-link class="link" to="/show">
+        <text class="title">Show</text>
+      </router-link>
+      <router-link class="link" to="/ask">
+        <text class="title">Ask</text>
+      </router-link>
+      <router-link class="link" to="/job">
+        <text class="title">Job</text>
+      </router-link>
+    </div>
   </div>
 </template>
 
-<style>
-  .app-header {
+<style scoped>
+  .header {
     position: relative;
     height: 120px;
     margin-bottom: 3px;
@@ -15,7 +33,7 @@
     border-bottom-color: #DDDDDD;
     background-color: #FF6600;
   }
-  .app-logo {
+  .logo {
     position: relative;
     width: 50px;
     height: 50px;
@@ -25,24 +43,37 @@
     border-style: solid;
     border-color: #FFFFFF;
   }
-  .app-title {
+  .image {
+    width: 44px;
+    height: 44px;
+  }
+  .nav {
+    display: flex;
     position: absolute;
     left: 120px;
-    top: 40px;
-    font-size: 42px;
-    line-height: 42px;
-    font-weight: bold;
+    top: 0;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: center;
+  }
+  .link {
+    display: flex;
+    align-items: center;
+    padding-left: 15px;
+    padding-right: 15px;
+    height: 118px;
+    flex-grow: 0;
+    border-bottom-width: 4px;
+    border-bottom-style: solid;
+    border-bottom-color: #FF6600;
+  }
+  .router-link-active {
+    border-bottom-color: #FFC198;
+  }
+  .title {
+    font-family: Verdana, Geneva, sans-serif;
+    font-size: 32px;
+    line-height: 44px;
     color: #FFFFFF;
   }
 </style>
-
-<script>
-  module.exports = {
-    props: {
-      title: {
-        type: String,
-        default: 'Hacker News'
-      }
-    }
-  }
-</script>
