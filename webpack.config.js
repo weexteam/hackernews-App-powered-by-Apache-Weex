f41bb6f13b1151f1e77d5ffdab6764f29c47eff9
Jinjiang <zhaojinjiang@me.com>
2017-01-05 20:31:26 +0800

    combo two webpack config together

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path')
+var webpack = require('webpack')
+
+var bannerPlugin = new webpack.BannerPlugin(
+  '// { "framework": "Vue" }\n',
+  { raw: true }
+)
+
+function getBaseConfig () {
+  return {
+    entry: {
+      'index': path.resolve('src', 'entry.js')
+    },
+    output: {
+      path: 'dist',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel',
+          exclude: /node_modules/
+        }, {
+          test: /\.vue(\?[^?]+)?$/,
+          loaders: []
+        }
+      ]
+    },
+    plugins: [bannerPlugin]
+  }
+}
+
+var webConfig = getBaseConfig()
+webConfig.output.filename = '[name].web.js'
+webConfig.module.loaders[1].loaders.push('vue')
+
+var weexConfig = getBaseConfig()
+weexConfig.output.filename = '[name].weex.js'
+weexConfig.module.loaders[1].loaders.push('weex')
+
+module.exports = [webConfig, weexConfig]
