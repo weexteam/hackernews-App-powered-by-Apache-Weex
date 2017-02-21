d6b672c45e566070d43b89e912058e9fb0c501a8
Hanks <zhanghan.me@gmail.com>
2017-02-21 23:49:04 +0800

    add comments about weex.config.env

--- src/components/external-link.vue
+++ src/components/external-link.vue
@@ -1,23 +1,25 @@
 <template>
-  <div @click="open"><slot></slot></div>
+  <div @click="open">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
   export default {
-    props: {
-      url: {
-        type: String,
-        default: ''
-      }
-    },
+    props: ['url'],
     methods: {
       open () {
-        if (this.$getConfig) {
-          this.jump(`/article/${this.url}`)
-        }
-        else {
+        // get the environment variables
+        const env = weex.config.env || WXEnvironment
+
+        // open a new window (tab) on the web
+        if (env.platform === 'Web') {
           window.open(this.url)
+          return
         }
+
+        // change router path on native (Android & iOS)
+        this.jump(`/article/${this.url}`)
       }
     }
   }
